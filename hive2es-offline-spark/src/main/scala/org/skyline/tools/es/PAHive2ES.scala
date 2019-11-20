c7c41eedc4c2a3eb355f4116f3a7d634a507885e
Sean Liu <wendaoheri@gmail.com>
2019-11-20 17:36:42 +0800

    add es plugin

--- hive2es-offline-spark/src/main/scala/org/skyline/tools/es/PAHive2ES.scala
+++ hive2es-offline-spark/src/main/scala/org/skyline/tools/es/PAHive2ES.scala
@@ -1,11 +1,15 @@
 package org.skyline.tools.es
 
+import org.skyline.tools.es.Hive2ES.Config
+
 
 object PAHive2ES {
 
 
   def main(args: Array[String]): Unit = {
-
+    val config = Config(id = "test")
+    val partitionKey = Option(config.routing).getOrElse(config.id)
+    print(partitionKey)
   }
 
 }
