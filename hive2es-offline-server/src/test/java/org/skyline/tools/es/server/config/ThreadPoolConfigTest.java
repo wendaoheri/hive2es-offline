4059d7d44cb7a8c7d2a4265b5bef70cd9f90679e
Sean Liu <wendaoheri@gmail.com>
2019-12-12 11:17:34 +0800

    add threadpool config

---
+++ hive2es-offline-server/src/test/java/org/skyline/tools/es/server/config/ThreadPoolConfigTest.java
@@ -0,0 +1,63 @@
+package org.skyline.tools.es.server.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.elasticsearch.client.transport.TransportClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyline.tools.es.server.ESClient;
+import org.skyline.tools.es.server.HdfsClient;
+import org.skyline.tools.es.server.IndicesChangeController;
+import org.skyline.tools.es.server.LeaderSelectorController;
+import org.skyline.tools.es.server.NodeService;
+import org.skyline.tools.es.server.RegistryCenter;
+import org.skyline.tools.es.server.TestEntry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Sean Liu
+ * @date 2019-12-12
+ */
+@SpringBootTest(classes = TestEntry.class)
+@RunWith(SpringRunner.class)
+@Slf4j
+public class ThreadPoolConfigTest {
+
+  @MockBean
+  private ESClient esClient;
+
+  @MockBean
+  private NodeService nodeService;
+
+  @MockBean
+  private RegistryCenter registryCenter;
+
+  @MockBean
+  private HdfsClient hdfsClient;
+
+  @MockBean
+  private IndicesChangeController indicesChangeController;
+
+  @MockBean
+  private LeaderSelectorController leaderSelectorController;
+
+  @MockBean
+  private CuratorFramework curatorFramework;
+
+  @MockBean
+  private TransportClient transportClient;
+
+  @Autowired
+  private ThreadPoolTaskExecutor downloadTaskExecutor;
+
+
+  @Test
+  public void test() {
+    assert downloadTaskExecutor != null;
+  }
+
+}
