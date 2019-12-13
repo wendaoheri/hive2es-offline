484d163c904c2d910ea11510645517bec77ec585
wendaoheri <wendaoheri@gmail.com>
2019-12-13 15:14:11 +0800

    delete unuse import

--- hive2es-offline-server/src/test/java/org/skyline/tools/es/server/TestEntry.java
+++ hive2es-offline-server/src/test/java/org/skyline/tools/es/server/TestEntry.java
@@ -2,9 +2,11 @@ package org.skyline.tools.es.server;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAutoConfiguration
 @ComponentScan({"org.skyline.tools.es"})
+@EnableAsync
 public class TestEntry {
 
 }
