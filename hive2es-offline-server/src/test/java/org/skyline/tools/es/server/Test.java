477e7870641dc54554ab474a2a39084b9d7cf67e
LiYuanZhao <liyuanzhao435@pingan.com.cn>
2020-04-13 13:45:13 +0800

    create index in spark application;

--- hive2es-offline-server/src/test/java/org/skyline/tools/es/server/Test.java
+++ hive2es-offline-server/src/test/java/org/skyline/tools/es/server/Test.java
@@ -1,10 +1,18 @@
 package org.skyline.tools.es.server;
 
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Test {
 
 
-    public static void main(String[] args) {
-        String str = "aaabbbb_dfdfd .. // wewewew custom_test_20191289";
-        System.out.println(str.contains("custom_test_20191289"));
+    public static void main(String[] args) throws UnknownHostException {
+        InetSocketTransportAddress node1 = new InetSocketTransportAddress(InetAddress.getByName("26.6.0.90"), 9300);
+        InetSocketTransportAddress node2 = new InetSocketTransportAddress(InetAddress.getByName("26.6.0.91"), 9300);
+
+        TransportClient transportClient = TransportClient.builder().build().addTransportAddresses(node1, node2);
     }
 }
---
+++ hive2es-offline-spark/src/main/scala/org/skyline/tools/es/ESClusterClient.scala
@@ -0,0 +1,60 @@
+package org.skyline.tools.es
+
+import java.net.InetAddress
+
+import com.alibaba.fastjson.JSONObject
+import org.apache.commons.logging.LogFactory
+import org.elasticsearch.client.transport.TransportClient
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.transport.InetSocketTransportAddress
+
+//spark driver使用的es client
+class ESClusterClient(val indexName: String,val shardsNum: Int,val typeName:String) {
+
+  @transient private lazy val log = LogFactory.getLog(getClass)
+
+  val node1 = new InetSocketTransportAddress(InetAddress.getByName("26.6.0.90"), 9300)
+  val node2 = new InetSocketTransportAddress(InetAddress.getByName("26.6.0.91"), 9300)
+  private val client: TransportClient = TransportClient.builder().build().addTransportAddresses(node1, node2)
+
+  def createIndex(): Boolean = {
+    val createRespon: Boolean = client.admin().indices().prepareCreate(indexName).setSettings((
+      s"""
+         |{
+         |    "index": {
+         |        "number_of_replicas": "0",
+         |        "refresh_interval": "-1",
+         |        "number_of_shards": "${shardsNum}",
+         |        "merge": {
+         |          "scheduler": {
+         |            "max_thread_count": "4",
+         |            "auto_throttle": "false"
+         |          },
+         |          "policy": {
+         |            "max_merged_segment": "2mb"
+         |          }
+         |        },
+         |        "translog":{
+         |          "flush_threshold_size": "10gb",
+         |          "durability": "async",
+         |          "sync_interval": "10m"
+         |        },
+         |        "routing":{
+         |          "allocation":{
+         |            "disable_allocation":"true"
+         |          }
+         |        }
+         |    }
+         |}
+         """.stripMargin)).get().isAcknowledged()
+    return createRespon
+  }
+
+  def putMapping2ESClusterIndex(mapping: JSONObject): Boolean = {
+    log.info("put mapping start")
+    val root = new JSONObject()
+    root.put("properties", mapping)
+    val result: Boolean = client.admin().indices().preparePutMapping(indexName).setType(typeName).setSource(root).execute().actionGet().isAcknowledged
+    return result
+  }
+}
